a5c1c7da0b2496ecdd95b68e3e50df6be8a7e552
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-13 10:16:35 +0530

    modified the program using class

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,62 +1,68 @@
-def get_tic_tac_toe_board(rows):
-    board = ''
-    current_row = [0, 3, 6]
-    for row in current_row:
-        board += "     |     |     \n"
-        board += f'  {rows[row]}  |  {rows[row + 1]}  |   {rows[row + 2]}  \n'
-        board += "     |     |     \n"
-        board += "- - - - - - - - -\n"
-    return board
-
-
-def change_player(current_player):
-    return int(not current_player)
-
-
-def get_player_name(current_player):
-    return 'player 1' if current_player == 0 else 'player 2'
-
-
-def get_position(current_player):
-    print(f'player : {get_player_name(current_player)}')
+def get_position():
     return int(input('Enter the position : '))
 
 
-def is_valid_position(entered_positions, position):
-    return position not in entered_positions and 0 <= position < 9
+class TicTacToe:
+    def __init__(self):
+        self.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.current_player = 2
+        self.is_won = False
+        self.entered_positions = []
+        self.chances = 1
+        self.symbols = {0: 'X', 1: 'O'}
+        self.win_conditions = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [4, 5, 6], [7, 8, 9], [3, 6, 9], [3, 5, 7]]
 
+    def get_board(self):
+        board = ''
+        current_row = [0, 3, 6]
+        for row in current_row:
+            board += "     |     |     \n"
+            board += f'  {self.rows[row]}  |  {self.rows[row + 1]}  |   {self.rows[row + 2]}  \n'
+            board += "     |     |     \n"
+            board += "- - - - - - - - -\n"
+        return board
 
-def is_won_the_game(symbol, rows):
-    win_conditions = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [4, 5, 6], [7, 8, 9], [3, 6, 9], [3, 5, 7]]
-    for condition in win_conditions:
-        if rows[condition[0] - 1] == symbol and rows[condition[1] - 1] == symbol and rows[condition[2] - 1] == symbol:
-            return True
-    return False
+    def change_player(self):
+        return int(not self.current_player)
 
+    def get_player_name(self):
+        return 'player 1' if self.current_player == 0 else 'player 2'
 
-def play(current_player, is_won, entered_positions, rows, chance):
-    if chance > 9:
-        return 'draw'
-    symbols = {0: 'X', 1: 'O'}
-    print(get_tic_tac_toe_board(rows))
-    if is_won:
-        return f'{get_player_name(current_player)} won the game🥳'
-    current_player = change_player(current_player)
-    position = get_position(current_player)
-    is_position_valid = is_valid_position(entered_positions, position - 1)
-    if is_position_valid:
-        entered_positions.append(position - 1)
-        rows[position - 1] = symbols[current_player]
-        is_won = is_won_the_game(symbols[current_player], rows)
-        return play(current_player, is_won, entered_positions, rows, chance + 1)
-    else:
-        print('\nThe entered position is not valid! Please enter a valid position')
-        return play(change_player(current_player), is_won, entered_positions, rows, chance)
+    def is_valid_position(self, position):
+        return position not in self.entered_positions and 0 <= position < 9
+
+    def is_won_the_game(self):
+        for condition in self.win_conditions:
+            symbol = self.symbols[self.current_player]
+            if self.rows[condition[0] - 1] == symbol and self.rows[condition[1] - 1] == symbol and self.rows[condition[2] - 1] == symbol:
+                return True
+        return False
+
+    def play(self):
+        if self.chances > 9:
+            return 'draw🙁'
+        print(self.get_board())
+        if self.is_won:
+            return f'{self.get_player_name()} won the game🥳'
+        self.current_player = self.change_player()
+        print(f'player : {self.get_player_name()}')
+        position = get_position()
+        is_position_valid = self.is_valid_position(position - 1)
+        if is_position_valid:
+            self.entered_positions.append(position - 1)
+            self.rows[position - 1] = self.symbols[self.current_player]
+            self.is_won = self.is_won_the_game()
+            self.chances += 1
+            return self.play()
+        else:
+            print('\nThe entered position is not valid! Please enter a valid position')
+            self.current_player = self.change_player()
+            return self.play()
 
 
 def main():
-    rows = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    game_status = play(2, False, [], rows, 1)
+    tic_tac_toe = TicTacToe()
+    game_status = tic_tac_toe.play()
     print(game_status)
 
 
